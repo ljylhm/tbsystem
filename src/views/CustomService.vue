696062144da7d444efb0064e28a2df55d9c7d0b6
ljylhm <948021695@qq.com>
2020-09-03 00:14:59 +0800

    # 提交客服工单

--- src/views/CustomService.vue
+++ src/views/CustomService.vue
@@ -1,8 +1,187 @@
 <template>
   <div class="custom-servive_container">
-      <div class="custom-header">
-          <div class="custom-header_item">新客服工单</div>
-      </div>
+    <div class="custom-header">
+      <div class="custom-header_item">新客服工单</div>
+    </div>
+
+    <el-tabs v-model="activeName" @tab-click="handleTabClick" class="custom-tab_container">
+      <el-tab-pane label="任务处罚列表" name="first">
+        <div class="custom-form">
+          <el-form :inline="true">
+            <el-form-item label="工单类型：">
+              <el-select v-model="form.orderType" placeholder="请选择" class="short-input">
+                <el-option
+                  v-for="item in orderTypes"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+
+            <el-form-item label="问题分类：">
+              <el-select v-model="form.questionType" placeholder="请选择" class="short-input">
+                <el-option
+                  v-for="item in questionTypes"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+
+            <el-form-item label="列表类型：">
+              <el-select v-model="form.statusType" placeholder="请选择" class="short-input">
+                <el-option
+                  v-for="item in statusType"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <br />
+
+            <el-form-item label="任务编号：">
+              <el-input v-model="form.missionId" class="short-input"></el-input>
+            </el-form-item>
+
+            <el-form-item label="订单编号：">
+              <el-input v-model="form.orderId" class="short-input"></el-input>
+            </el-form-item>
+
+            <el-form-item label="订单时间：">
+              <el-date-picker
+                v-model="form.create_time"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+              ></el-date-picker>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type="primary" round>查询</el-button>
+            </el-form-item>
+          </el-form>
+
+          <div>
+            <el-table :data="customServicePublishData">
+              <el-table-column label="任务编号" prop="missionId" align="center" width="200px"></el-table-column>
+              <el-table-column label="订单编号" prop="orderId" align="center" width="200px"></el-table-column>
+              <el-table-column label="工单类型" prop="orderType" align="center" width="200px"></el-table-column>
+              <el-table-column label="问题分类" prop="questionType" align="center" width="180px"></el-table-column>
+
+              <el-table-column label="处罚金额" prop="pubishNumber" align="center" width="180px"></el-table-column>
+
+              <el-table-column label="工单状态" prop="status" align="center" width="180px"></el-table-column>
+              <el-table-column label="创建时间" prop="create_time" align="center"></el-table-column>
+            </el-table>
+          </div>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="多退少补列表" name="second">
+        <div class="custom-form">
+          <el-form :inline="true">
+            <el-form-item label="任务编号：">
+              <el-input v-model="formOne.missionId" class="short-input"></el-input>
+            </el-form-item>
+
+            <el-form-item label="订单编号：">
+              <el-input v-model="formOne.orderId" class="short-input"></el-input>
+            </el-form-item>
+            <br/>
+
+            <el-form-item label="列表类型：">
+              <el-select v-model="formOne.statusType" placeholder="请选择" class="short-input">
+                <el-option
+                  v-for="item in statusType"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            
+
+            <el-form-item label="订单时间：">
+              <el-date-picker
+                v-model="formOne.create_time"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+              ></el-date-picker>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type="primary" size="small">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+
+        <div>
+          <el-table :data="moreFixData">
+            <el-table-column label="任务编号" prop="missionId" align="center" width="200px"></el-table-column>
+            <el-table-column label="订单编号" prop="orderId" align="center" width="200px"></el-table-column>
+            <el-table-column label="担保金额" prop="orderType" align="center" width="200px"></el-table-column>
+            <el-table-column label="支付金额" prop="questionType" align="center" width="180px"></el-table-column>
+            <el-table-column label="工单状态" prop="status" align="center" width="180px"></el-table-column>
+            <el-table-column label="创建时间" prop="create_time" align="center"></el-table-column>
+          </el-table>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="发货提醒列表" name="third">
+        <div class="custom-form">
+          <el-form :inline="true">
+            <el-form-item label="任务编号：">
+              <el-input v-model="formTwo.missionId" class="short-input"></el-input>
+            </el-form-item>
+
+            <el-form-item label="订单编号：">
+              <el-input v-model="formTwo.orderId" class="short-input"></el-input>
+            </el-form-item>
+            <br/>
+            
+            <el-form-item label="列表类型：">
+              <el-select v-model="formTwo.statusType" placeholder="请选择" class="short-input">
+                <el-option
+                  v-for="item in statusType"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            
+
+            <el-form-item label="订单时间：">
+              <el-date-picker
+                v-model="formTwo.create_time"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+              ></el-date-picker>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type="primary" size="small">查询</el-button>
+            </el-form-item>
+          </el-form>
+
+          <div>
+            <el-table :data="sendWranData">
+              <el-table-column label="任务编号" prop="missionId" align="center" width="200px"></el-table-column>
+              <el-table-column label="订单编号" prop="orderId" align="center" width="200px"></el-table-column>
+              <el-table-column label="任务状态" prop="orderstatus" align="center" width="180px"></el-table-column>
+              <el-table-column label="工单状态" prop="status" align="center" width="180px"></el-table-column>
+              <el-table-column label="创建时间" prop="create_time" align="center"></el-table-column>
+            </el-table>
+          </div>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -18,56 +197,51 @@ import Header from "@/components/Header.vue"; // @ is an alias to /src
   },
 })
 export default class Publish extends Vue {
-  // 展示模态框的集合
-  showSearchShopModal = true;
-
-  status = "first";
-
-  shopInfo = {
-    name: "",
-    title: "",
-    shop_name: "",
-    link: "",
-    image: "",
-  };
+  activeName = "first";
 
   form = {
-    price: 1,
-    checkBuyer: "2",
-    needUpImage: "2",
-    publishType: "1",
+    orderType: "",
+    questionType: "",
+    listType: "",
+    missionId: "",
+    orderId: "",
+    create_time: "",
   };
 
-  tableData = [
-    {
-      flowType: "",
-      keyWord: "",
-      num: "",
-    },
-  ];
+  formOne = {
+    missionId: "",
+    orderId: "",
+    statusType: "",
+    create_time: "",
+  };
 
-  dateDate = [{}];
+  formTwo = {
+    missionId: "",
+    orderId: "",
+    statusType: "",
+    create_time: "",
+  };
 
-  flowTypes = [
+  orderTypes = [
     {
       value: "1",
-      label: "APP自然搜索",
+      label: "订单信息错误",
     },
     {
       value: "2",
-      label: "PC自然搜索",
+      label: "好评问题",
     },
     {
       value: "3",
-      label: "APP淘口令",
+      label: "其他导致卖家损失的行为",
     },
     {
       value: "4",
-      label: "APP直通车",
+      label: "任务过程出错",
     },
     {
       value: "5",
-      label: "PC直通车",
+      label: "提醒卖家发货",
     },
     {
       value: "6",
@@ -75,40 +249,76 @@ export default class Publish extends Vue {
     },
   ];
 
-  shopOptions = [
+  questionTypes = [
     {
       value: "1",
-      label: "淘宝",
-      children: [
-        {
-          value: "11",
-          label: "耐克",
-        },
-        {
-          value: "12",
-          label: "阿迪",
-        },
-      ],
+      label: "问题一",
+    },
+    {
+      value: "2",
+      label: "问题二",
     },
   ];
 
-  searchShopForm = {
-    type: "",
-  };
+  statusType = [
+    {
+      value: "1",
+      label: "待处理",
+    },
+    {
+      value: "2",
+      label: "跟进中",
+    },
+    {
+      value: "3",
+      label: "处理完成",
+    },
+    {
+      value: "4",
+      label: "已撤销",
+    },
+    {
+      value: "5",
+      label: "拒绝处理",
+    },
+  ];
 
-  addFromSetting = () => {
-    const o = {
-      flowType: "",
-      keyWord: "",
-      num: "",
-    };
-    this.tableData.push(o);
-  };
+  customServicePublishData = [
+    {
+      missionId: "123321",
+      orderId: "13213",
+      orderType: "-",
+      questionType: "-",
+      pubishNumber: 60,
+      status: "-",
+      create_time: "-",
+    },
+  ];
 
-  deleteFromSetting = (index: number) => {
-    if (this.tableData.length == 1) return;
-    this.tableData.splice(index, 1);
-  };
+  moreFixData = [
+    {
+      missionId: "123321",
+      orderId: "13213",
+      warrantNumber: "123",
+      payNumber: "123321",
+      status: "",
+      create_time: "",
+    },
+  ];
+
+  sendWranData = [
+    {
+      missionId: "123321",
+      orderId: "13213",
+      status: "",
+      orderstatus: "",
+      create_time: "",
+    },
+  ];
+
+  handleTabClick(tab: string) {
+    console.log(tab, event);
+  }
 }
 </script>
 
@@ -141,24 +351,37 @@ export default class Publish extends Vue {
   margin-left: 15px;
 }
 
-.custom-servive_container{
-    width: 1200px;
-    margin: 0 auto;
-    padding-top: 40px;
-    .custom-header{
-        width: 100%;
-        height: 50px;
-        line-height: 50px;
-        font-size: 14px;
-        background: #eee;
-        text-align: left;
-        .custom-header_item{
-            display: inline-block;
-            text-align: center;
-            padding: 0px 20px;
-            background: #4782ef;
-            color: #fff;
-        }
+.custom-servive_container {
+  width: 1400px;
+  margin: 0 auto;
+  padding-top: 40px;
+  .custom-header {
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    font-size: 14px;
+    background: #eee;
+    text-align: left;
+    .custom-header_item {
+      display: inline-block;
+      text-align: center;
+      padding: 0px 20px;
+      background: #4782ef;
+      color: #fff;
     }
+  }
+
+  .custom-tab_container {
+    margin: 10px 0px;
+  }
+
+  .custom-form {
+    margin: 10px 0px;
+    text-align: left;
+    .short-input {
+      width: 150px;
+      margin-right: 10px;
+    }
+  }
 }
 </style>
